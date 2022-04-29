6598c072487ffb6467ccabb40fd84eb49b9520e9
gbengajolayemi <oluwagbengajolayemi@gmail.com>
2022-04-29 20:15:28 +0100

    commit

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -4,7 +4,7 @@
             <div class="row no-collapse align-items-center">
                 <div class="col">
                     <router-link to="/" class="logo" :aria-label="$t('view_home.back_to_home')">
-                        <img src="fantom-logo.svg" alt="" class="not-fluid" />
+                        <img src="fantom-loggo.svg" alt="" class="not-fluid" />
                     </router-link>
                 </div>
             </div>
