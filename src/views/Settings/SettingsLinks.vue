922591a0f4851afacda4a299943d2b4eb3d4cf6f
gbengajolayemi <oluwagbengajolayemi@gmail.com>
2022-04-29 16:52:46 +0100

    commit

---
+++ src/views/Settings/SettingsLinks.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="settingslinks">
+        <f-card class="f-card-double-padding">
+            <div class="small-container">
+                <p>
+                    <a href="https://fantom.foundation/how-to-use-fantom-wallet/" rel="noopener" target="_blank">
+                        How to use GO fWallet
+                    </a>
+                </p>
+
+                <p>
+                    <a href="https://explorer.fantom.network/" rel="noopener" target="_blank"> Fantom Explorer </a>
+                </p>
+
+                <social-media-links />
+            </div>
+        </f-card>
+    </div>
+</template>
+
+<script>
+import SocialMediaLinks from '@/components/SocialMediaLinks/SocialMediaLinks.vue';
+import FCard from '@/components/core/FCard/FCard.vue';
+
+export default {
+    name: 'SettingsLinks',
+
+    components: { FCard, SocialMediaLinks },
+};
+</script>
